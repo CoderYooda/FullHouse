fd4358086c433b7f58aa28970d6eb776992f9ef7
s.senatorov <s.senatorov@adengi.ru>
2025-12-13 16:26:50 +0300

    base

---
+++ src/resources/js/actions/App/Http/Controllers/Auth/AuthController.ts
@@ -0,0 +1,85 @@
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../../wayfinder'
+/**
+* @see \App\Http\Controllers\Auth\AuthController::login
+* @see app/Http/Controllers/Auth/AuthController.php:13
+* @route '/login'
+*/
+export const login = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: login.url(options),
+    method: 'get',
+})
+
+login.definition = {
+    methods: ["get","head"],
+    url: '/login',
+} satisfies RouteDefinition<["get","head"]>
+
+/**
+* @see \App\Http\Controllers\Auth\AuthController::login
+* @see app/Http/Controllers/Auth/AuthController.php:13
+* @route '/login'
+*/
+login.url = (options?: RouteQueryOptions) => {
+    return login.definition.url + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\Auth\AuthController::login
+* @see app/Http/Controllers/Auth/AuthController.php:13
+* @route '/login'
+*/
+login.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: login.url(options),
+    method: 'get',
+})
+
+/**
+* @see \App\Http\Controllers\Auth\AuthController::login
+* @see app/Http/Controllers/Auth/AuthController.php:13
+* @route '/login'
+*/
+login.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
+    url: login.url(options),
+    method: 'head',
+})
+
+/**
+* @see \App\Http\Controllers\Auth\AuthController::login
+* @see app/Http/Controllers/Auth/AuthController.php:13
+* @route '/login'
+*/
+const loginForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: login.url(options),
+    method: 'get',
+})
+
+/**
+* @see \App\Http\Controllers\Auth\AuthController::login
+* @see app/Http/Controllers/Auth/AuthController.php:13
+* @route '/login'
+*/
+loginForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: login.url(options),
+    method: 'get',
+})
+
+/**
+* @see \App\Http\Controllers\Auth\AuthController::login
+* @see app/Http/Controllers/Auth/AuthController.php:13
+* @route '/login'
+*/
+loginForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: login.url({
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'HEAD',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'get',
+})
+
+login.form = loginForm
+
+const AuthController = { login }
+
+export default AuthController

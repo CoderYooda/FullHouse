850e38850560f39af6b71db92d1b642c5344550a
mihail <mihail@webstyle.top>
2026-03-31 20:33:34 +0300

    awd

--- src/database/migrations/2026_03_24_191511_change_participants_serialnumber_default.php
+++ src/database/migrations/2026_03_24_191511_change_participants_serialnumber_default.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,8 +12,9 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::statement('ALTER TABLE "participants" ALTER COLUMN "serial_number" TYPE integer USING "serial_number"::integer');
         Schema::table('participants', function (Blueprint $table) {
-            $table->string('serial_number')->nullable()->default(null)->change();
+            $table->integer('serial_number')->nullable()->default(null)->change();
         });
     }
 
@@ -21,6 +23,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('participants', function (Blueprint $table) {
+            $table->string('serial_number')->nullable()->default(null)->change();
+        });
     }
 };

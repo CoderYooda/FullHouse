be4dc928804785aa25bd038f04129695b6eeb198
tracer2008 <mihail@webstyle.top>
2026-04-26 15:58:11 +0300

    awd

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -28,6 +28,7 @@
         if (!window.IsWeb) {
             Telegram.WebApp.lockOrientation();
         }
+        window.TelegramBotUsername = '{{ env('TELEGRAM_BOT_USERNAME') }}';
     </script>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i" />

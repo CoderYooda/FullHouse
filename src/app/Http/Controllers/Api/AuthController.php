f71759bbea10be794d0e4d222a4771b6f35ff248
tracer2008 <mihail@webstyle.top>
2026-04-29 22:18:55 +0300

    test

--- src/app/Http/Controllers/Api/AuthController.php
+++ src/app/Http/Controllers/Api/AuthController.php
@@ -94,7 +94,6 @@ class AuthController extends Controller
 
     public function telegramLogin(Request $request)
     {
-        \Log::info('Full request data', $request->all());
         // Временно отключаем проверку хеша для теста
          $this->validateTelegramHash($request);
 

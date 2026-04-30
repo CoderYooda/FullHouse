2cdaeb68604c3c8234e5cabe215bfb227f62bb77
tracer2008 <mihail@webstyle.top>
2026-04-30 22:41:03 +0300

    test

--- src/resources/js/pages/LoginTelegram.vue
+++ src/resources/js/pages/LoginTelegram.vue
@@ -30,11 +30,18 @@ export default {
       axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     },
 
-    async handleTelegramLogin(initData) {
+    async handleTelegramLogin(user) {
+      console.log('Telegram user data:', user);
       this.isLoading = true;
       try {
         const { data } = await axios.post('/api/telegram-login', {
-          init_data: initData,  // ← отправляем всю строку initData
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          username: user.username,
+          photo_url: user.photo_url,
+          auth_date: user.auth_date,
+          hash: user.hash,
         });
 
         localStorage.setItem('_token', data.token);
@@ -42,10 +49,7 @@ export default {
         this.$store.commit('auth/SET_USER', data.user);
 
         this.setAxiosAuthHeader(data.token);
-        this.$router.push({
-          name: 'player_telegram',
-          params: { slug: this.$route.params.slug || window.Slug || 'blg' }
-        });
+        this.$router.push({ name: 'player_telegram', params: { slug: this.$route.params.slug || window.Slug || 'blg' } });
       } catch (error) {
         console.error(error);
         this.error = error.response?.data?.message || 'Ошибка входа через Telegram';
@@ -55,15 +59,32 @@ export default {
     },
   },
   mounted() {
-    const webApp = window.Telegram?.WebApp;
-    if (webApp?.initData) {
-      // Отправляем на сервер строку initData
-      this.handleTelegramLogin(webApp.initData);
+    if (window.Telegram?.WebApp?.initData) {
+      const initData = window.Telegram.WebApp.initData;
+      const user = window.Telegram.WebApp.initDataUnsafe?.user;
+
+      if (user && user.id) {
+        const data = {
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          username: user.username,
+          photo_url: user.photo_url,
+          auth_date: window.Telegram.WebApp.initDataUnsafe?.auth_date,
+          hash: initData
+        };
+        console.log('приветики', data)
+        console.log('приветики2', initData)
+        this.handleTelegramLogin(data);
+      } else {
+        this.error = 'Не удалось получить данные пользователя';
+        this.isLoading = false;
+      }
     } else {
       this.error = 'Интерфейс доступен только через Telegram MiniApp';
       this.isLoading = false;
     }
-  },
+  }
 
 
   // methods: {

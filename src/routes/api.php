c71bac34d0cd6b78bb44c1636405f30488eebdd7
tracer2008 <mihail@webstyle.top>
2026-05-02 14:55:37 +0300

    test

--- src/routes/api.php
+++ src/routes/api.php
@@ -35,7 +35,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/feedback/create', [FeedbackController::class, 'receiveFeedback']);
 
     Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/link-telegram', [AuthController::class, 'linkTelegram']);
+    Route::match(['get', 'post'], '/link-telegram', [AuthController::class, 'linkTelegram']);
     Route::post('/unlink-telegram', [AuthController::class, 'unlinkTelegram']);
     Route::post('/set-city', [UserController::class, 'setCity']);
 
